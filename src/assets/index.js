099973149d58c321371f46825d043d220fd2ddbe
nielraja123 <nielraja123@gmail.com>
2023-06-25 13:29:15 +0700

    Basic Components with Hooks and Register Page Slicing

---
+++ src/assets/index.js
@@ -0,0 +1,3 @@
+import RegisterBg from "./image/register-bg.jpg";
+import LoginBg from "./image/login-bg.jpg";
+export { RegisterBg, LoginBg };
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,9 +1,28 @@
 import React from "react";
+import { RegisterBg } from "../../assets";
+import "./register.scss";
+import "../../index.css";
+import { Gap, Input, Link } from "../../components";
+import Button from "../../components/atoms/Button";
 
 const Register = () => {
   return (
-    <div>
-      <p>Register Page</p>
+    <div className="main-page">
+      <div className="left">
+        <img src={RegisterBg} className="bg-image" alt="imgBg" />
+      </div>
+      <div className="right">
+        <p className="title">Register</p>
+        <Input label="Full Name" placeholder="Fullname" />
+        <Gap height={18} />
+        <Input label="Email" placeholder="Email" />
+        <Gap height={18} />
+        <Input label="Password" placeholder="Password" />
+        <Gap height={50} />
+        <Button title="Register" />
+        <Gap height={100} />
+        <Link title="Kembali ke Halaman Login" />
+      </div>
     </div>
   );
 };
